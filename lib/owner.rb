172084b26934259a625d36d560d810a2bb63d9ec
Stephen Sanchez <stephensanchez05@gmail.com>
2019-12-20 20:03:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,4 +64,11 @@ class Owner
     self.dogs.each { |dog| dog.mood = "happy" }
   end
   
+  def feed_cats
+    self.cats.each { |cat| cat.mood = "happy" }
+  end
+  
+  def sell_pets
+    self.dogs.each { |dog| dog.mood = "happy" }
+  
 end
